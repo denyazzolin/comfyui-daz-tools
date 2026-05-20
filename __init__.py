17d1eb5024de3cb927f6453deb3b5458718a91d2
denyazzolin <denny.azzolin@gmail.com>
2026-05-20 13:56:45 -0400

    Update init print message

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-print("[DAZ Tools] Loaded nodes: Check Null, Null Audio Checker, Abs Int")
+print("[DAZ TOOLS] - Initializing Daz tool nodes")
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
